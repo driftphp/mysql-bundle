47bfbcf8dfe7a17c96f1db511783a4c60241ccdd
Marc Morera <yuhu@mmoreram.com>
2019-12-26 11:26:54 +0100

    Fixed parameter name + style

--- DependencyInjection/CompilerPass/MysqlCompilerPass.php
+++ DependencyInjection/CompilerPass/MysqlCompilerPass.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of the Drift Http Kernel
+ * This file is part of the Drift Project
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -33,7 +33,7 @@ class MysqlCompilerPass implements CompilerPassInterface
      */
     public function process(ContainerBuilder $container)
     {
-        $connectionsConfiguration = $container->getParameter('mysql.clients_configuration');
+        $connectionsConfiguration = $container->getParameter('mysql.connections_configuration');
         if (empty($connectionsConfiguration)) {
             return;
         }
